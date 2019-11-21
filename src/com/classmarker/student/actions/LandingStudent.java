1c64891ce5f28687c2aa0f6c3a2a70ca8b451865
Shubham Gupta <32667635+Shubham1204@users.noreply.github.com>
2019-11-21 22:29:58 +0530

    code

--- src/com/classmarker/student/actions/LandingStudent.java
+++ src/com/classmarker/student/actions/LandingStudent.java
@@ -35,7 +35,7 @@ public class LandingStudent {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		con = CommonDAO.getConnection();
-		pstmt = con.prepareStatement("select distinct(test_mst.testname),group_mst.groupname,subject_mst.subjectname  from test_mst,branch_mst,group_mst,user_role_mapping,role_mst,subject_branch_semester_mapping,group_student_mapping,subject_mst,semester_mst,userteacher_branch_semester_subject_mapping,test_branch_semester_subject_group_mapping,user_mst where test_branch_semester_subject_group_mapping.groupid=group_student_mapping.groupid and test_branch_semester_subject_group_mapping.testid=test_mst.testid and  subject_branch_semester_mapping.subjectid=test_branch_semester_subject_group_mapping.subjectid and user_role_mapping.roleid=role_mst.roleid and role_mst.rolename='student' and user_mst.uid=?");
+		pstmt = con.prepareStatement("select distinct(test_mst.testname),group_mst.groupname,subject_mst.subjectname  from test_mst,branch_mst,group_mst,user_role_mapping,role_mst,subject_branch_semester_mapping,group_student_mapping,subject_mst,semester_mst,userteacher_branch_semester_subject_mapping,test_branch_semester_subject_group_mapping,user_mst where test_branch_semester_subject_group_mapping.groupid=group_student_mapping.groupid and test_branch_semester_subject_group_mapping.testid=test_mst.testid and  subject_branch_semester_mapping.subjectid=test_branch_semester_subject_group_mapping.subjectid and user_role_mapping.roleid=role_mst.roleid and role_mst.rolename='student' and group_mst.groupid=group_student_mapping.groupid and group_student_mapping.studentid=user_mst.uid and user_mst.uid=?");
 		pstmt.setString(1,uniqueid);
 		rs = pstmt.executeQuery();
 		if(rs.next()) {
